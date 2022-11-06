7be33a87768dbe7e2f57841ab265b91e4e9f97a2
cristianbyte <charlsmaritz@gmail.com>
2022-11-06 16:53:33 -0500

    count down listo

--- js/tetis.js
+++ js/tetis.js
@@ -1,10 +1,32 @@
 const downArrowFather = document.getElementsByClassName('tracklsit-planet')[0]
 const ufo = document.getElementsByClassName('rawu_ufo_random')[0];
+const dayHourMinSec = document.getElementsByClassName('countDown__boxSon-timer')
+
+const currentDate = new Date().getFullYear()
+const relaseDate = new Date(`November 11 ${currentDate} 00:00:00`)
+let transistor = ''
+
+function updateCountTimer(){
+    const currentTime = new Date();
+    const diff = relaseDate - currentTime;
+
+    transistor = Math.floor( diff/1000 / 60 / 60 / 24)
+    dayHourMinSec[0].innerHTML = transistor
+    transistor = Math.floor( diff/1000 / 60 / 60) % 24
+    dayHourMinSec[1].innerHTML = transistor
+    transistor = Math.floor( diff/1000 / 60 ) % 60
+    dayHourMinSec[2].innerHTML = transistor
+    transistor = Math.floor( diff/1000 ) % 60
+    dayHourMinSec[3].innerHTML = transistor
+}
+
+setInterval(() => {
+    updateCountTimer()
+}, 1000);
 
 function toggle(e){
     if(e.target && e.target.tagName === 'A'){
         e.target.classList.toggle('active')
-        // e.target.style.height = 'auto'
     }
 }
 function ufoMove(e){
