03e2aba7e7ba52f0abec43e5f5e36dfc41e2abb6
cristianbyte <charlsmaritz@gmail.com>
2022-11-07 00:50:02 -0500

    finished website

--- js/tetis.js
+++ js/tetis.js
@@ -1,29 +1,29 @@
 const downArrowFather = document.getElementsByClassName('tracklsit-planet')[0]
 const ufo = document.getElementsByClassName('rawu_ufo_random')[0];
 const dayHourMinSec = document.getElementsByClassName('countDown__boxSon-timer')
+const loader = document.querySelector(".loader");
 
 const currentDate = new Date().getFullYear()
 const relaseDate = new Date(`November 11 ${currentDate} 00:00:00`)
 let transistor = ''
+
+function disableScroll(){  
+    window.scrollTo(0, 0);
+} 
 
 function updateCountTimer(){
     const currentTime = new Date();
     const diff = relaseDate - currentTime;
 
     transistor = Math.floor( diff/1000 / 60 / 60 / 24)
-    dayHourMinSec[0].innerHTML = transistor
+    dayHourMinSec[0].innerHTML = transistor < 10 ? '0' + transistor : transistor
     transistor = Math.floor( diff/1000 / 60 / 60) % 24
-    dayHourMinSec[1].innerHTML = transistor
+    dayHourMinSec[1].innerHTML = transistor < 10 ? '0' + transistor : transistor
     transistor = Math.floor( diff/1000 / 60 ) % 60
-    dayHourMinSec[2].innerHTML = transistor
+    dayHourMinSec[2].innerHTML = transistor < 10 ? '0' + transistor : transistor
     transistor = Math.floor( diff/1000 ) % 60
-    dayHourMinSec[3].innerHTML = transistor
+    dayHourMinSec[3].innerHTML = transistor < 10 ? '0' + transistor : transistor
 }
-
-setInterval(() => {
-    updateCountTimer()
-}, 1000);
-
 function toggle(e){
     if(e.target && e.target.tagName === 'A'){
         e.target.classList.toggle('active')
@@ -40,6 +40,13 @@ function videoEnd(e){
     }
 }
 
+window.addEventListener("load", function(){
+    loader.className += " hidden";
+    window.removeEventListener('scroll', disableScroll); 
+})
+
+setInterval(() => {updateCountTimer()}, 1000);
+window.addEventListener('scroll', disableScroll);
 downArrowFather.addEventListener('click',toggle)
 window.addEventListener('mousemove',ufoMove)
 
